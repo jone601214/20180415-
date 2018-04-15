2a57a1333aa9f64f9acedbb2ec1857d983c97fea
jone601214 <32056035+jone601214@users.noreply.github.com>
2018-04-15 23:41:26 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    public int data;
+    public Node(int data1){
+        this.setData(data1);
+    }
+public void setData(int data1){
+        this.data=data1;
+}
+public int getData() {
+    return data;
+   }
+}
